5d31d25752be736f23e48382b3849749a2d1edea
MackenzieWangenstein <mwangenstein82@gmail.com>
2017-07-03 15:46:40 -0700

    created Shelter table for pdxheo app

--- pdxheo/models.py
+++ pdxheo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Shelters(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    address = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    hours = models.CharField(max_length=200)
