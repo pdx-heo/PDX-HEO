838da3d0cfa78265645f9349ec83a16340b6b630
MackenzieWangenstein <mwangenstein82@gmail.com>
2017-07-08 15:16:11 -0700

    created very basic skeleton structure of web app

--- pdxheo/views.py
+++ pdxheo/views.py
@@ -8,9 +8,25 @@ from .models import Organization
 
 
 def index(request):
+    return render(request, 'pdxheo/index.html')
+
+def safetymap(request):
+    return render(request, 'pdxheo/safetymap.html')
+
+def communityboard(request):
+    return render(request, 'pdxheo/communityboard.html')
+
+def testimonies(request):
+    return render(request, 'pdxheo/testimonies.html')
+
+def about(request):
+    return render(request, 'pdxheo/about.html')
+
+
+def finder(request):
     shelter_list = Shelter.objects.order_by('name')[:10]
     context = {'shelter_list': shelter_list}
-    return render(request, 'pdxheo/index.html', context)
+    return render(request, 'pdxheo/finder.html', context)
 
 
 def organization(request, organization_id):
@@ -21,4 +37,3 @@ def organization(request, organization_id):
 def shelter(request, shelter_id):
     she = get_object_or_404(Shelter, pk=shelter_id)
     return render(request, 'pdxheo/shelter.html', {'shelter': she})
-
