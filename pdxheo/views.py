4cdacb32553f46ec16443d5c0e0c8c228131ce9b
Andy Mayer <mayermic@pdx.edu>
2017-07-09 17:04:47 -0700

    Refactored some URLs

--- pdxheo/views.py
+++ pdxheo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.template import loader
 
