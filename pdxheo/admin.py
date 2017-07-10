c61f0a2408186af37cfef57e374ffdd2855e3a6d
Andy Mayer <mayermic@pdx.edu>
2017-07-09 18:46:15 -0700

    Updated admin site

--- pdxheo/admin.py
+++ pdxheo/admin.py
@@ -3,5 +3,29 @@ from .models import Organization
 from .models import Shelter
 
 
-admin.site.register(Organization)
-admin.site.register(Shelter)
+class ShelterInline(admin.TabularInline):
+  model = Shelter
+  extra = 1
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+  fieldsets = [
+    ('Organization Details', {'fields': ['name', 'description', 'address', 'hours_open', 'hours_close']}),
+    ('Publish', {'fields': ['pub_date'], 'classes': ['collapse']}),
+  ]
+  inlines = [ShelterInline]
+  list_display = ('name', 'address', 'pub_date')
+  list_filter = ['pub_date']
+  search_fields = ['name']
+
+
+class ShelterAdmin(admin.ModelAdmin):
+  fieldsets = [
+    ('Organization', {'fields': ['organization']}),
+    ('Shelter Details', {'fields': ['name', 'description', 'address', 'hours_open', 'hours_close']}),
+    ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+  ]
+
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Shelter, ShelterAdmin)
