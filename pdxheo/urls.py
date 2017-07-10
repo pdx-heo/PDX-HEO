16e1012861965f8c4be28159b00b05aa824c58fd
Andy Mayer <mayermic@pdx.edu>
2017-07-09 17:44:10 -0700

    Updated some views to take advantage of Django generics

--- pdxheo/urls.py
+++ pdxheo/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     url(r'^about/', views.about, name='about'),
     url(r'^communityboard/', views.communityboard, name='communityboard'),
     url(r'^community/', views.communityboard, name='communityboard'),
-    url(r'^finder/', views.finder, name='finder'),
+    url(r'^finder/', views.FinderView.as_view(), name='finder'),
     url(r'^safetymap/', views.safetymap, name='safetymap'),
     url(r'^safety/', views.safetymap, name='Safety Map'),
     url(r'^testimonies/', views.testimonies, name='testimonies'),
     # ex: /pdxheo/organization/1/
-    url(r'^organization/(?P<organization_id>[0-9]+)/$', views.organization, name='organization'),
+    url(r'^organization/(?P<pk>[0-9]+)/$', views.OrganizationView.as_view(), name='organization'),
     # ex: /pdxheo/shelter/1/
-    url(r'^shelter/(?P<shelter_id>[0-9]+)/$', views.shelter, name='shelter'),
+    url(r'^shelter/(?P<pk>[0-9]+)/$', views.ShelterView.as_view(), name='shelter'),
 ]
--- pdxheo/views.py
+++ pdxheo/views.py
@@ -1,36 +1,45 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
+from django.views import generic
 
 from .models import Shelter
 from .models import Organization
 
 
 def index(request):
-    return render(request, 'pdxheo/index.html')
+  return render(request, 'pdxheo/index.html')
+
 
 def safetymap(request):
-    return render(request, 'pdxheo/safetymap.html')
+  return render(request, 'pdxheo/safetymap.html')
+
 
 def communityboard(request):
-    return render(request, 'pdxheo/communityboard.html')
+  return render(request, 'pdxheo/communityboard.html')
+
 
 def testimonies(request):
-    return render(request, 'pdxheo/testimonies.html')
+  return render(request, 'pdxheo/testimonies.html')
+
 
 def about(request):
-    return render(request, 'pdxheo/about.html')
+  return render(request, 'pdxheo/about.html')
 
-def finder(request):
-    shelter_list = Shelter.objects.order_by('name')[:10]
-    context = {'shelter_list': shelter_list}
-    return render(request, 'pdxheo/finder.html', context)
 
-def organization(request, organization_id):
-    org = get_object_or_404(Organization, pk=organization_id)
-    return render(request, 'pdxheo/organization.html', {'organization': org})
+class FinderView(generic.ListView):
+  template_name = 'pdxheo/finder.html'
+  context_object_name = 'shelter_list'
 
-def shelter(request, shelter_id):
-    she = get_object_or_404(Shelter, pk=shelter_id)
-    return render(request, 'pdxheo/shelter.html', {'shelter': she})
+  def get_queryset(self):
+    return Shelter.objects.order_by('name')[:10]
+
+
+class OrganizationView(generic.DetailView):
+  model = Organization
+  template_name = 'pdxheo/organization.html'
+
+
+class ShelterView(generic.DetailView):
+  model = Shelter
+  template_name = 'pdxheo/shelter.html'
