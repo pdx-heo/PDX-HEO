3a079549eaf0ccaafed32fa42ec986de9c073836
Andy Mayer <mayermic@pdx.edu>
2017-07-11 21:31:17 -0700

    Fixed some spelling and formatting

--- pdxheo/views.py
+++ pdxheo/views.py
@@ -12,6 +12,7 @@ from .models import Shelter
 from .models import Organization
 from .serializers import OrganizationSerializer
 
+
 def index(request):
   return render(request, 'pdxheo/index.html')
 
@@ -44,58 +45,59 @@ class OrganizationView(generic.DetailView):
   model = Organization
   template_name = 'pdxheo/organization.html'
 
-@api_view(['GET','PUT','DELETE'])
-def organization(request, organization_id, formate=None):
-    #old version
 
-    #org = get_object_or_404(Organization, pk=organization_id)
-    #return render(request, 'pdxheo/organization.html', {'organization': org})
+@api_view(['GET', 'PUT', 'DELETE'])
+def organization(request, organization_id, format=None):
+  # old version
 
-    #new version
+  # org = get_object_or_404(Organization, pk=organization_id)
+  # return render(request, 'pdxheo/organization.html', {'organization': org})
 
-    try:
-        organization = Organization.objects.get(organization_id=organization_id)
-    except Organization.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+  # new version
 
-        if request.method == 'GET':
-            serializer = OrganizationSerializer(organization)
-            return Response(serializer.data)
+  try:
+    organization = Organization.objects.get(organization_id=organization_id)
+  except Organization.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
 
-        elif request.method == 'PUT':
-             serializer = OrganizationSerializer(organization, data=request.data)
-             if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  if request.method == 'GET':
+    serializer = OrganizationSerializer(organization)
+    return Response(serializer.data)
 
-        elif request.method == 'DELETE':
-            organization.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+  elif request.method == 'PUT':
+    serializer = OrganizationSerializer(organization, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  elif request.method == 'DELETE':
+    organization.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # example: http://127.0.0.1:8000/organization.json  # JSON suffix
-# example http://127.0.0.1:8000/organization.api   # Browsable API suffi
-@api_view(['GET','POST'])
+# example http://127.0.0.1:8000/organization.api   # Browsable API suffix
+@api_view(['GET', 'POST'])
 def organization_list(request, format=None):
-    """
-    List all organizations or create a new organizatoin
-    """
-    if request.method == 'GET':
-        organizations = Organization.objects.all()
-        serializer = OrganizationSerializer(organizations, many=True)
-        return Response(serializer.data)
+  """
+  List all organizations or create a new organization
+  """
+  if request.method == 'GET':
+    organizations = Organization.objects.all()
+    serializer = OrganizationSerializer(organizations, many=True)
+    return Response(serializer.data)
 
-    elif request.method == 'POST':
-    #    data = JSONParser().parse(request)
-        serializer = OrganizationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  elif request.method == 'POST':
+    # data = JSONParser().parse(request)
+    serializer = OrganizationSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-                    #return JsonResponse(serializer.data, status=201)
-            #    return JsonResponse(serializer.errors, status=400)
+    # return JsonResponse(serializer.data, status=201)
+    # return JsonResponse(serializer.errors, status=400)
 
 
 class ShelterView(generic.DetailView):
