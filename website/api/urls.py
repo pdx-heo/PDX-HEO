6c830ab968e115cc113a6f17818840f16ff30ec5
MackenzieWangenstein <mwangenstein82@gmail.com>
2017-07-13 21:24:41 -0700

    updated api view to use classes

--- website/api/urls.py
+++ website/api/urls.py
@@ -4,7 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-  url(r'^organizations/$', views.organization_list),
+  url(r'^organization/$', views.OrganizationList.as_view(), name='organization list'),
+  url(r'^organization/(?P<pk>[0-9]+)/$', views.OrganizationDetail.as_view(), name='organization'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- website/api/views.py
+++ website/api/views.py
@@ -1,57 +1,53 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+from django.http import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from website.models import Organization, Shelter
 from .serializers import OrganizationSerializer
 
+class OrganizationDetail(APIView):
+  """
+  """
+  def get_object(self, pk):
+      try:
+          return Organization.objects.get(pk=pk)
+      except Organization.DoesNotExist:
+          raise Http404
 
-# Create your views here.
-@api_view(['GET', 'PUT', 'DELETE'])
-def organization(request, organization_id, format=None):
-  try:
-    organization = Organization.objects.get(organization_id=organization_id)
-  except Organization.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-  if request.method == 'GET':
+  def get(self, request, pk, format=None):
+    organization = self.get_object(pk)
     serializer = OrganizationSerializer(organization)
     return Response(serializer.data)
 
-  elif request.method == 'PUT':
+  def put(self, request, pk, format=None):
+    organization = self.get_object(pk)
     serializer = OrganizationSerializer(organization, data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-  elif request.method == 'DELETE':
+  def delete(self, request, pk, format=None):
+    organization = self.get_object(pk)
     organization.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # example: http://127.0.0.1:8000/organizations.json  # JSON suffix
 # example http://127.0.0.1:8000/organizations.api   # Browsable API suffix
-@api_view(['GET', 'POST'])
-def organization_list(request, format=None):
+class OrganizationList(APIView):
   """
   List all organizations or create a new organization
   """
-  if request.method == 'GET':
-    organizations = Organization.objects.all()
-    serializer = OrganizationSerializer(organizations, many=True)
-    return Response(serializer.data)
+  def get(self, request, format=None):
+      organizations = Organization.objects.all()
+      serializer = OrganizationSerializer(organizations, many=True)
+      return Response(serializer.data)
 
-  elif request.method == 'POST':
-    # data = JSONParser().parse(request)
+  def post(self, request, format=None):
     serializer = OrganizationSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # return JsonResponse(serializer.data, status=201)
-    # return JsonResponse(serializer.errors, status=400)
