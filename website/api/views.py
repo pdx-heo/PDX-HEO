2658483c6f1446cd8d19a9388377701895746c2e
MackenzieWangenstein <mwangenstein82@gmail.com>
2017-07-13 21:36:37 -0700

    finished organization api

--- website/api/views.py
+++ website/api/views.py
@@ -1,53 +1,19 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
 from website.models import Organization, Shelter
 from .serializers import OrganizationSerializer
+from rest_framework import generics
 
-class OrganizationDetail(APIView):
+class OrganizationDetail(generics.RetrieveUpdateDestroyAPIView):
   """
+    Create get,post, put, delete organization
   """
-  def get_object(self, pk):
-      try:
-          return Organization.objects.get(pk=pk)
-      except Organization.DoesNotExist:
-          raise Http404
-
-  def get(self, request, pk, format=None):
-    organization = self.get_object(pk)
-    serializer = OrganizationSerializer(organization)
-    return Response(serializer.data)
-
-  def put(self, request, pk, format=None):
-    organization = self.get_object(pk)
-    serializer = OrganizationSerializer(organization, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-  def delete(self, request, pk, format=None):
-    organization = self.get_object(pk)
-    organization.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+  queryset = Organization.objects.all()
+  serializer_class = OrganizationSerializer
 
 
-# example: http://127.0.0.1:8000/organizations.json  # JSON suffix
-# example http://127.0.0.1:8000/organizations.api   # Browsable API suffix
-class OrganizationList(APIView):
+class OrganizationList(generics.ListCreateAPIView):
+
   """
   List all organizations or create a new organization
   """
-  def get(self, request, format=None):
-      organizations = Organization.objects.all()
-      serializer = OrganizationSerializer(organizations, many=True)
-      return Response(serializer.data)
-
-  def post(self, request, format=None):
-    serializer = OrganizationSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  queryset = Organization.objects.all()
+  serializer_class = OrganizationSerializer
