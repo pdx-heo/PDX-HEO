daa080db60fcb89651b200ce8a0c09c55b1fc2a9
Andy Mayer <mayermic@pdx.edu>
2017-07-18 20:39:52 -0700

    Moved API urls to a new API namespace

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
@@ -15,4 +15,5 @@ urlpatterns = [
   url(r'^organization/(?P<pk>[0-9]+)/$', views.OrganizationView.as_view(), name='organization'),
   # ex: /pdxheo/shelter/1/
   url(r'^service/(?P<pk>[0-9]+)/$', views.ServiceView.as_view(), name='service'),
+  url(r'^api/', include('website.api.urls', namespace="api")),
 ]
