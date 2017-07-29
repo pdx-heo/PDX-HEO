a03a1c53d31d062b98153e7b22b4201bcf2ed4b6
MackenzieWangenstein <mwangenstein82@gmail.com>
2017-07-28 18:13:59 -0700

    replaced community board link with link to forum app

--- forum/forum/signals.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-
-import django.dispatch
-
-
-forum_moved = django.dispatch.Signal(providing_args=["previous_parent", ])
-forum_viewed = django.dispatch.Signal(providing_args=["forum", "user", "request", "response", ])
--- forum/forum_member/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-
-from machina.apps.forum_member.abstract_models import AbstractForumProfile
-from machina.core.db.models import model_factory
-
-
-ForumProfile = model_factory(AbstractForumProfile)
