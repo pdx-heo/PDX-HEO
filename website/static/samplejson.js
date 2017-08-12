8df49a8d6d5cf97189d141477b45132c256897d9
Chitra M <Chitram2@pdx.edu>
2017-08-11 21:38:50 -0700

    Initial commit

---
+++ website/static/samplejson.js
@@ -0,0 +1,41 @@
+mapfeed_callback(
+    {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-122.676030,  45.520131],
+                    "address" : "Columbia Shelter,1234 NW Orchid st"
+                },
+            },
+//FB 2
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-122.588535, 45.558038],
+                    "address" : "Oregon food bank,5678 NW Orchid st"
+                },
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-122.779268, 45.439817],
+                    "address" : "Portland shelter 2222 NW Orchid st"
+                },
+            },
+
+//Food bank
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-122.632166,45.580300],
+                   "address" : "Food bank 1425 cosmos st"
+                },
+            },
+        ]
+    });
