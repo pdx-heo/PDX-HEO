1fcf969416e5160c119182d5ffc705d96236b715
Chitra M <Chitram2@pdx.edu>
2017-08-17 16:58:40 -0700

    coords

--- website/static/mapcoords.js
+++ website/static/mapcoords.js
@@ -7,7 +7,7 @@ mapfeed_callback(
                 "geometry": {
                     "type": "Point",
                     "coordinates": [-122.676030, 45.520131],
-                    "address": "Columbia Shelter,509 SE Grand Ave"
+                    "address": "Columbia Shelter,509 SE Grand Ave,Portland, OR 97214"
                 },
             },
 
@@ -16,15 +16,15 @@ mapfeed_callback(
                 "geometry": {
                     "type": "Point",
                     "coordinates": [-122.588535, 45.558038],
-                    "address": "Oregon food bank,7900 NE 33rd Dr"
+                    "address": "Oregon food bank,7900 NE 33rd Dr, Portland, OR 97211"
                 },
             },
             {
                 "type": "Feature",
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [-122.779268, 45.439817],
-                    "address": "Portland shelter 2222 NW Orchid st"
+                    "coordinates": [-122.671571, 45.523295],
+                    "address": "Portland Rescue Mission 111 W. Burnside Portland, OR 97209"
                 },
             },
 
@@ -33,9 +33,19 @@ mapfeed_callback(
                 "type": "Feature",
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [-122.632166, 45.580300],
-                    "address": "Food bank 1425 cosmos st"
+                    "coordinates": [-122.677288, 45.527697],
+                    "address": "Transition Projects 475 NW Glisan Street Portland, OR 97209"
                 },
             },
+
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-122.576058, 45.438675],
+                    "address": "Salvation Army 8495 SE Monterey Avenue Happy Valley, OR 97086"
+                },
+            },
+
         ]
     });
